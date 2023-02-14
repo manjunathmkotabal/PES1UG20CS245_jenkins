8aaf2638284d38a10178debb7eebfaecb32c2319
MANJUNATH KOTABAL <manjunathmkotabal99@gmail.com>
2023-02-14 13:00:31 +0530

    second one

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(void) {
-  cout << "PES1UG20CS245\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout<<"what's up jenkins\n";
+  cout<<"manjunath : PES1UG20CS245\n";
   return 0;
 }
